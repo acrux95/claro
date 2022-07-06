6141724f29de23d07d5d4a2abf1ab0bf41537872
Armando Cruz <acrux95.code@gmail.com>
2022-07-06 18:58:57 -0500

    feat: building header component

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,10 @@
+import './Header.scss';
+
+const Header = () => {
+  return(
+    <div className="logo">
+      <img src="https://www.clarovideo.com/webclient/sk_core/images/clarovideo-logo-sitio.svg" alt="Claro Video logo" />
+    </div>
+  )
+};
+export {Header};
