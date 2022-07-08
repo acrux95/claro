663f65f57bf9c87b3b6845c39088a04b6b02a91c
Armando Cruz <acrux95.code@gmail.com>
2022-07-08 10:06:12 -0500

    feat: creating details screen

---
+++ src/Modules/Details/index.tsx
@@ -0,0 +1,15 @@
+import './Details.scss';
+
+const Details = () => {
+  return (
+    <section className="details">
+      <div className="details_image">
+        <img src="https://clarovideocdn1.clarovideo.net/PELICULAS/BOXTHE/EXPORTACION_WEB/SS/BOXTHEWHORIZONTAL.jpg?size=675x380" alt="" />
+      </div>
+      <div className="details_description">
+        <p>Description</p>
+      </div>
+    </section>
+  )
+}
+export {Details};
