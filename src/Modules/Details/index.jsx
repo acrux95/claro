84d93ec8e1ce4cc77b1783fcd0bb199a1dc8d938
Armando Cruz <acrux95.code@gmail.com>
2022-07-10 14:54:45 -0500

    fix: fixing slint format

--- src/Modules/Details/index.jsx
+++ src/Modules/Details/index.jsx
@@ -13,7 +13,7 @@ function Details({ selected, details, setDetails }) {
     const fetchVideos = async (id) => {
       try {
         const detailsRes = await axios.get(
-          `https://mfwkweb-api.clarovideo.net/services/content/data?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=web62c5d605af24a&user_id=59352999&group_id=${id}`
+          `https://mfwkweb-api.clarovideo.net/services/content/data?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=web62c5d605af24a&user_id=59352999&group_id=${id}`,
         );
         setDetails(detailsRes.data.response.group.common);
         console.log(details);
@@ -33,21 +33,16 @@ function Details({ selected, details, setDetails }) {
         <section className="details_section_description">
           <div>
             <p>{details.large_description}</p>
-            <p>
-              {`${details.extendedcommon.media.publishyear} duración: ${details.duration}`}
-            </p>
+            <p>{`${details.extendedcommon.media.publishyear} duración: ${details.duration}`}</p>
             <div className="details_section_description_actors">
-              {details.extendedcommon.roles.role[0].talents.talent.map(
-                (actor) => (
-                  <p>{actor.fullname}</p>
-                )
-              )}
+              {details.extendedcommon.roles.role[0].talents.talent.map((actor) => (
+                <p>{actor.fullname}</p>
+              ))}
             </div>
             <p>Director:</p>
             <p>Escritor:</p>
-            <p>Productor</p>
+            <p>Productor:</p>
             <p>Género:</p>
-            
           </div>
         </section>
       </section>
--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const getVideos = () => {
   axios
     .get(
-      "https://mfwkweb-api.clarovideo.net/services/content/list?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=8ssspdg7rjufgbgk823q1irqv1&quantity=50&from=0&level_id=GPS&order_way=ASC&order_id=50&filter_id=34270"
+      "https://mfwkweb-api.clarovideo.net/services/content/list?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=8ssspdg7rjufgbgk823q1irqv1&quantity=50&from=0&level_id=GPS&order_way=ASC&order_id=50&filter_id=34270",
     )
     .then((res) => res.data.response.groups)
     .catch((err) => console.log(err));
@@ -14,7 +14,7 @@ export const getDetails = (id) => {
   console.log(id);
   axios
     .get(
-      `https://mfwkweb-api.clarovideo.net/services/content/data?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=web62c5d605af24a&user_id=59352999&group_id=${id}`
+      `https://mfwkweb-api.clarovideo.net/services/content/data?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=web62c5d605af24a&user_id=59352999&group_id=${id}`,
     )
     .then((res) => res.data.response.group.common)
     .catch((err) => console.log(err));
