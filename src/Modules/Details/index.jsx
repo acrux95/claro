aa2103fc5d88c293aee0dce2eea6ae504cc1e225
Armando Cruz <acrux95.code@gmail.com>
2022-07-12 12:34:07 -0500

    fix: adding key to components

--- src/Modules/Details/index.jsx
+++ src/Modules/Details/index.jsx
@@ -17,9 +17,8 @@ function Details({ selected, details, setDetails }) {
           `https://mfwkweb-api.clarovideo.net/services/content/data?device_id=web&device_category=web&device_model=web&device_type=web&device_so=Chrome&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.93&region=mexico&HKS=web62c5d605af24a&user_id=59352999&group_id=${id}`,
         );
         // eslint-disable-next-line no-unused-expressions, no-undefined
-        const detailsResp = detailsRes.data.response.group.common;
-        console.log(detailsResp);
-        setDetails(detailsResp);
+        setDetails(detailsRes.data.response.group.common);
+        console.log(detailsRes);
       } catch (error) {
         console.log(error);
       }
@@ -34,41 +33,43 @@ function Details({ selected, details, setDetails }) {
         <div className="details_section_image">
           <img src={details.image_large} alt={details.title} />
         </div>
-        <section className="details_section_description">
-          <div>
-            <p>{details.large_description}</p>
-            <p>{`${details.extendedcommon.media.publishyear} Duración: ${details.duration}`}</p>
-            <div className="details_section_description_info">
-              {details.extendedcommon.roles.role[0].talents.talent.map((actor) => (
-                <p>{actor.fullname}</p>
-              ))}
+        {details.extendedcommon && (
+          <section className="details_section_description">
+            <div>
+              <p>{details.large_description}</p>
+              <p>{`${details.extendedcommon.media.publishyear} Duración: ${details.duration}`}</p>
+              <div className="details_section_description_info">
+                {details.extendedcommon.roles.role[0].talents.talent.map((actor) => (
+                  <p key={actor.id}>{actor.fullname}</p>
+                ))}
+              </div>
+              <div className="details_section_description_info">
+                <p className="details_section_description_info_talent">Director:</p>
+                {details.extendedcommon.roles.role[1].talents.talent.map((dir) => (
+                  <p key={dir.id}>{dir.fullname}</p>
+                ))}
+              </div>
+              <div className="details_section_description_info">
+                <p className="details_section_description_info_talent">Escritor:</p>
+                {details.extendedcommon.roles.role[2].talents.talent.map((esc) => (
+                  <p key={esc.id}>{esc.fullname}</p>
+                ))}
+              </div>
+              <div className="details_section_description_info">
+                <p className="details_section_description_info_talent">Productor:</p>
+                {details.extendedcommon.roles.role[3].talents.talent.map((prod) => (
+                  <p key={prod.id}>{prod.fullname}</p>
+                ))}
+              </div>
+              <div className="details_section_description_info">
+                <p className="details_section_description_info_talent">Género:</p>
+                {details.extendedcommon.genres.genre.map((gen) => (
+                  <p key={gen.id}>{gen.name}</p>
+                ))}
+              </div>
             </div>
-            <div className="details_section_description_info">
-              <p className="details_section_description_info_talent">Director:</p>
-              {details.extendedcommon.roles.role[1].talents.talent.map((dir) => (
-                <p>{dir.fullname}</p>
-              ))}
-            </div>
-            <div className="details_section_description_info">
-              <p className="details_section_description_info_talent">Escritor:</p>
-              {details.extendedcommon.roles.role[2].talents.talent.map((esc) => (
-                <p>{esc.fullname}</p>
-              ))}
-            </div>
-            <div className="details_section_description_info">
-              <p className="details_section_description_info_talent">Productor:</p>
-              {details.extendedcommon.roles.role[3].talents.talent.map((prod) => (
-                <p>{prod.fullname}</p>
-              ))}
-            </div>
-            <div className="details_section_description_info">
-              <p className="details_section_description_info_talent">Género:</p>
-              {details.extendedcommon.genres.genre.map((gen) => (
-                <p>{gen.name}</p>
-              ))}
-            </div>
-          </div>
-        </section>
+          </section>
+        )}
       </section>
     </section>
   );
