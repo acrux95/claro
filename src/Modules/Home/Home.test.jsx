ce50c74262f34afc60ef27cfe7312e65cae68db2
Armando Cruz <acrux95.code@gmail.com>
2022-07-12 11:50:19 -0500

    test: changing test

--- src/Modules/Home/Home.test.jsx
+++ src/Modules/Home/Home.test.jsx
@@ -1,15 +1,16 @@
-import "@testing-library/jest-dom";
-import { fireEvent, screen } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import Home from "./index";
 import mock from "../../mocks/response.json";
 
-describe('Component: Home Verification', () => {
+test("component: Home Verification", () => {
   const props = {
-    videos, search, setVideos, setSelected, setSearch
-  }
-  it('Should render correctly', async () => {
-    render(<Home {...props} />);
-    
-  })
-})
+    videos,
+    search,
+    setVideos,
+    setSelected,
+    setSearch,
+  };
+  const component = render(<Home videos={videos} />)
+  console.log
+});
