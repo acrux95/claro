ac7c4bf23788d2688b4b9889749caaf4d6d08934
Armando Cruz <acrux95.code@gmail.com>
2022-07-12 19:44:18 -0500

    test: fixing test

--- src/Modules/Home/Home.test.jsx
+++ src/Modules/Home/Home.test.jsx
@@ -1,16 +1,44 @@
+import configureStore from "redux-mock-store";
 import "@testing-library/jest-dom/extend-expect";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
 import Home from "./index";
 import mock from "../../mocks/response.json";
 
-test("component: Home Verification", () => {
-  const props = {
-    videos,
-    search,
-    setVideos,
-    setSelected,
-    setSearch,
-  };
-  const component = render(<Home videos={videos} />)
-  console.log
+const videos = mock.response.groups;
+const mockStore = configureStore([]);
+
+describe("test suite", () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore({
+      videos: mock.response.groups,
+      search: mock.response.groups[0].title,
+    });
+
+    store.dispatch = jest.fn();
+
+    component = renderer.create(
+      <Provider store={store}>
+        <Home />
+      </Provider>,
+    );
+  });
+
+  it("should render with given state from Redux store", () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("should dispatch an action on button click", () => {
+    renderer.act(() => {
+      component.root.findByType("button").props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith(myAction({ payload: "sample text" }));
+  });
+  
 });
